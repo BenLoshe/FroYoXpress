3b4915d96fb010673cf2974175ff2f541d550584
Benjamin J Loshe <bjloshe@purdue.edu>
2020-06-04 21:52:26 -0400

    navbar is finally done- no mixing templates

--- js/myNav.js
+++ js/myNav.js
@@ -13,6 +13,6 @@ function myFunction() {
   if (window.pageYOffset >= sticky) {
     navbar.classList.add("sticky")
   } else {
-    navbar.classList.remove("sticky");
+    // navbar.classList.remove("sticky");
   }
 }
